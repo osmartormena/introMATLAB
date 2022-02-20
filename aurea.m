e38c81b2d5dafe9302cb48bd5315081e4032b858
tormena <tormena@localhost>
2022-02-20 19:07:58 -0300

    Otimizado para precisão de máquina.

--- aurea.m
+++ aurea.m
@@ -1,9 +1,11 @@
 % Cálculo da razão áurea (phi)
 
 phi = 3;            % chute inicial
+tmp = -1;           % variável temporária
 iter = uint8(0);    % contador de iterações (0–255)
 
-while phi ~= sqrt(phi + 1)
+while abs(phi - tmp) > eps(phi)
+    tmp = phi;
     phi = sqrt(phi + 1);
     iter = iter + 1;
 end
