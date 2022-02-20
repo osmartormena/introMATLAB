1a4490bd304207f1b827e5000b58146ce338d2a5
tormena <tormena@localhost>
2022-02-20 18:53:50 -0300

    Otimizado com while-loop.

--- aurea.m
+++ aurea.m
@@ -1,10 +1,12 @@
 % Cálculo da razão áurea (phi)
 
-phi = 3;        % chute inicial
-n = uint8(32);  % número de iterações (0–255)
+phi = 3;            % chute inicial
+iter = uint8(0);    % contador de iterações (0–255)
 
-for iter = 1:n
+while phi ~= sqrt(phi + 1)
     phi = sqrt(phi + 1);
-    str = [num2str(iter,'%u'),'ª iteração: ϕ = ',num2str(phi,'%1.15f')];
-    disp(str)
+    iter = iter + 1;
 end
+
+str = [num2str(iter,'%u'),' iterações: ϕ = ',num2str(phi,'%1.15f')];
+disp(str)
