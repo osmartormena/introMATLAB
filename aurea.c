a41b927871e4ea09f722b25148caf740ae737f8a
tormena <tormena@localhost>
2022-02-28 17:04:24 -0300

    Função interna.

--- aurea.c
+++ aurea.c
@@ -4,20 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void golden(double *phi, unsigned int *iter);
+
 int main() {
 
-	double phi = 3;
-	double tmp = -1;
+		double phi = 3;
+		unsigned int iter = 0;
 
-	unsigned int iter = 0;
+		golden(&phi, &iter);
 
-	while (fabs(phi - tmp) > DBL_EPSILON) {
-		tmp = phi;
-		phi = sqrt(phi + 1);
-		iter++;
-	}
+		printf("%u iterações: ϕ = %1.15f\n",iter,phi);
+		return EXIT_SUCCESS;
 
-	printf("%u iterações: ϕ = %1.15f\n",iter,phi);
-	return EXIT_SUCCESS;
+}
+
+void golden(double *phi, unsigned int *iter) {
+
+		double tmp = -1;
+
+		while (fabs(*phi - tmp) > DBL_EPSILON) {
+				tmp = *phi;
+				*phi = sqrt(*phi + 1);
+				*iter++;
+		} 
 
 }
--- aurea.m
+++ aurea.m
@@ -1,6 +1,15 @@
 % Cálculo da razão áurea (phi)
 
 phi = 3;            % chute inicial
+
+[phi, iter] = golden(phi);
+
+str = [num2str(iter, '%u'), ' iterações: ϕ = ', num2str(phi, '%1.15f')];
+disp(str)
+
+%% Função golden
+function [phi, iter] = golden(phi)
+
 tmp = -1;           % variável temporária
 iter = uint8(0);    % contador de iterações (0–255)
 
@@ -10,5 +19,4 @@ while abs(phi - tmp) > eps(phi)
     iter = iter + 1;
 end
 
-str = [num2str(iter, '%u'), ' iterações: ϕ = ', num2str(phi, '%1.15f')];
-disp(str)
+end
